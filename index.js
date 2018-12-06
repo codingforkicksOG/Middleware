b7a95c282352f7b2daacefdd30e1956ec80b82d9
Kendra Williams <kmonique.williams@gmail.com>
2018-12-05 23:23:07 -0500

    changed port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const supplierRouter = require("./supplier_router");
 const itemRouter = require("./item_router");
 
 const server = express();
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
 
 server.use(
    express.json(),
